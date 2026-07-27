cfcd0773732bdddc0b8bba14880932e009bd5381
DinoMito1 <njchiaravallo@gmail.com>
2026-07-26 23:44:07 -0400

    Finished Menu

--- Scenes/title_screen.gd
+++ Scenes/title_screen.gd
@@ -3,13 +3,27 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	$Background/Carnival1.visible = true
+	$Background/Carnival2.visible = false
+	$Background/Carnival3.visible = false
+	# timer for animation will auto start
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
+func _on_timer_timeout() -> void:
+	if $Background/Carnival1.visible == true:
+		$Background/Carnival2.visible = true
+		$Background/Carnival1.visible = false
+	
+	elif $Background/Carnival2.visible == true:
+		$Background/Carnival3.visible = true
+		$Background/Carnival2.visible = false
+	
+	elif $Background/Carnival3.visible == true:
+		$Background/Carnival1.visible = true
+		$Background/Carnival3.visible = false
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/level_scene.tscn")
