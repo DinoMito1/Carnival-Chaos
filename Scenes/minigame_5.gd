d0f056d00e30fde95c3a6cbba59d90b59f1bc707
DinoMito1 <njchiaravallo@gmail.com>
2026-09-01 14:09:32 -0400

    Minigame5HammerSwing

--- Scenes/minigame_5.gd
+++ Scenes/minigame_5.gd
@@ -2,6 +2,8 @@ extends Node2D
 @onready var won = false
 @onready var swung = false
 
+@onready var buttonHit1 = preload("res://Sprites/HammerButtonHit1.png")
+@onready var buttonHit2 = preload("res://Sprites/HammerButtonHit2.png")
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,7 +21,7 @@ func _ready() -> void:
 	await $ThemedTimer.Timer(4)
 	
 	$TimeTickingSound.stop()
-	if won == false:
+	if won == false or swung == false:
 		Global.lost_prev = true
 		Global.lives -= 1
 		Global.minigames_done -= 1
@@ -35,5 +37,22 @@ func _process(delta: float) -> void:
 		var pos = $Hammer.position
 		$Hammer/AnimationPlayer.stop()
 		$Hammer.position = pos
-		print('swing down')
+		$Hammer/AnimationPlayer.play("hammer_swing")
+		await get_tree().create_timer(.9).timeout
 		
+		if won == false: # if you missed the button
+			#lose the minigame
+			Global.lost_prev = true
+			Global.lives -= 1
+			Global.minigames_done -= 1
+			if Global.lives > 0:
+				get_tree().change_scene_to_file("res://Scenes/level_scene.tscn")
+			else:
+				get_tree().change_scene_to_file("res://Scenes/lose_screen.tscn")
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	#when hammer collides with button
+	won = true
+	$Button.texture = buttonHit1
+	await get_tree().create_timer(.2 ).timeout
+	$Button.texture = buttonHit2
